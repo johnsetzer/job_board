184fd5aff1735fd53f8d1699d7af19709f5445c1
John Setzer <john.setzer@gmail.com>
2011-05-10 11:57:42 -0400

    Devise ignoring views

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(resource)
+    redirect_to home_new_user_path
+  end
+end
+
---
+++ db/migrate/20110510133750_add_name_to_user.rb
@@ -0,0 +1,11 @@
+class AddNameToUser < ActiveRecord::Migration
+  def self.up
+    add_column :users, :name, :string, :limit => 50, :null => false
+  end
+
+  def self.down
+    remove_column :users, :name
+  end
+end
+
+
